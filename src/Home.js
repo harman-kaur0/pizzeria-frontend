08024d921c580a760f26f621c45408dd5055cc31
Harmandeep Kaur <harmandeepk628@gmail.com>
2022-05-01 21:43:30 -0700

    created nav bar

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div class="home-container">
+            <div class="home-text">
+                <h1>Exercise?<br/>I thought you said <i>extra slice</i>!</h1>
+                <p>freshly made pizza and pasta</p>
+            </div>
+            <div class="home-pizza-img">
+                <img src="https://www.seekpng.com/png/full/0-6914_free-icons-png-pizza-png.png" alt="pizza"/>
+            </div>
+        </div>
+    )
+};
+
+export default Home;
